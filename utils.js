57fea15c41ea5d944cfc2baa5c041cde74f27ee6
Gregory Koberger <gkoberger@gmail.com>
2017-03-22 14:12:17 -0700

    No need for quotes

--- utils.js
+++ utils.js
@@ -243,7 +243,7 @@ exports.swaggerInlineExample = function(_lang) {
 
   var annotation = [
     '@api [get] /pet/{petId}',
-    'description: "Returns all pets from the system that the user has access to"',
+    'description: Returns all pets from the system that the user has access to',
     'parameters:',
     '  - (path) petId=2* {Integer} The pet ID',
     '  - (query) limit {Integer:int32} The number of resources to return',
