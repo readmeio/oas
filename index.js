cffcb799e805ae5c43fdeefc8f401056a7999c4b
Gregory Koberger <gkoberger@gmail.com>
2018-05-28 17:52:33 -0700

    Prettier support

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #! /usr/bin/env node
-var _ = require('lodash');
+var _ = require("lodash");
 
-var parseArgs = require('minimist')(process.argv.slice(2))
+var parseArgs = require("minimist")(process.argv.slice(2));
 var args = parseArgs._;
 var opts = _.clone(parseArgs);
-delete opts['_'];
+delete opts["_"];
 
-require('./api').api(args, opts);
+require("./api").api(args, opts);
