57bbc1161ef873365f7bc3a208f7952dd5f240b6
Jon Ursenbach <jon@ursenba.ch>
2019-11-12 14:55:50 -0800

    chore: exposing another library

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const Oas = require('./src/oas');
-const { Operation } = require('./src/oas');
 
 module.exports = Oas;
-module.exports.Operation = Operation;
--- utils.js
+++ utils.js
@@ -1,9 +1,11 @@
+const findSchemaDefinition = require('./src/lib/find-schema-definition');
+const getPath = require('./src/lib/get-path');
 const getSchema = require('./src/lib/get-schema');
 const parametersToJsonSchema = require('./src/lib/parameters-to-json-schema');
-const findSchemaDefinition = require('./src/lib/find-schema-definition');
 
 module.exports = {
+  findSchemaDefinition,
+  getPath,
   getSchema,
   parametersToJsonSchema,
-  findSchemaDefinition,
 };
