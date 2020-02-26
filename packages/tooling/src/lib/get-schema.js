370c5ef135c9b843d1c1bdecb5a5fdc6c59188e7
Jon Ursenbach <erunion@users.noreply.github.com>
2020-02-26 11:01:37 -0800

    fix: updating our handling of JSON Schema to be compliant against allowEmptyValue (#110)
    * fix: rewriting our jsonschema construction for parameters to account for empty defaults
    
    * chore: rewriting some obtuse handling of common parameters
    
    * test: moving a test expectation into a snapshot
    
    * test: reorganizing the json schema tests because they were getting out of control
    
    * style: set the root eslintrc files as the roots
    
    * fix: moving our empty string default support over to allowEmptyValue compliance
    
    * fix: adding some more json schema test cases for parameters
    
    * fix: greatly simplifying the json schema fixture
    
    * test: adding more test cases for json schema in request bodies
    
    * test: adding $oneOf test cases for request bodies
    
    * test: more polymorphism test cases
    
    * feat: fleshing out the work to cleanup defaults in requestBody schemas
    
    * style: removing unused test helpers

--- packages/tooling/src/lib/get-schema.js
+++ packages/tooling/src/lib/get-schema.js
@@ -1,7 +1,8 @@
 const findSchemaDefinition = require('./find-schema-definition');
 
 // Gets the schema of the first media type defined in the `content` of the path operation
-// or returns the ref if there's no Request Body Object
+// or returns the ref if there's no Request Body Object.
+//
 // If the ref looks like a `requestBodies` reference, then do a lookup for the actual schema
 // https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#fixed-fields-8
 module.exports = function getSchema(pathOperation, oas) {
@@ -15,7 +16,10 @@ module.exports = function getSchema(pathOperation, oas) {
       return getSchema({ requestBody: findSchemaDefinition(pathOperation.requestBody.$ref, oas) });
     }
 
-    return { type: 'application/json', schema: pathOperation.requestBody };
+    return {
+      type: 'application/json',
+      schema: pathOperation.requestBody,
+    };
   } catch (e) {} // eslint-disable-line no-empty
 
   return undefined;
