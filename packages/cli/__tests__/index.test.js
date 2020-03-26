cabc018299b0bcc7939ca92bbbdd407003ce79d8
Jon Ursenbach <jon@ursenba.ch>
2020-03-26 14:27:54 -0700

    chore: decoupling oas-tooling from the cli package

--- packages/cli/__tests__/index.test.js
+++
@@ -1,7 +0,0 @@
-const Oas = require('../src/index');
-
-describe('index.js', () => {
-  it('should be a simple pass through of `@readme/oas-tooling`', () => {
-    expect(Object.keys(Oas)).toStrictEqual(['Operation']);
-  });
-});
--- packages/cli/src/utils.js
+++
@@ -1,3 +0,0 @@
-const utils = require('@readme/oas-tooling/utils');
-
-module.exports = utils;
