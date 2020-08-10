7eacf8181703379987723eebfa1025602cca431b
Jon Ursenbach <jon@ursenba.ch>
2020-08-10 15:41:28 -0700

    fix: path location issue when run from an npm package

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
+const path = require('path');
 
 const exp = {};
-fs.readdirSync('./src')
+fs.readdirSync(path.join(__dirname, './src'))
   .filter(har => {
     return har.indexOf('.har') !== -1;
   })
   .forEach(har => {
-    exp[har] = JSON.parse(fs.readFileSync(`./src/${har}`, 'utf8'));
+    exp[har] = JSON.parse(fs.readFileSync(path.join(__dirname, `./src/${har}`), 'utf8'));
   });
 
 module.exports = exp;
