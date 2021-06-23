582221f1f6e89eb99011c752f6cd67b62b8af10c
darrenyong <darren@readme.io>
2021-06-23 13:09:19 -0700

    fix: update getParmaeters to pass parameter name as currentLocation in constructSchema

--- src/operation/get-parameters-as-json-schema.js
+++ src/operation/get-parameters-as-json-schema.js
@@ -483,7 +483,8 @@ function getParameters(path, operation, oas, globalDefaults) {
       let schema = {};
       if ('schema' in current) {
         schema = {
-          ...(current.schema ? constructSchema(current.schema, [], '', globalDefaults) : {}),
+          ...(current.schema ? constructSchema(current.schema, [], `/${current.name}`, globalDefaults) : {}),
+          // ...(current.schema ? constructSchema(current.schema, [], '', globalDefaults) : {}),
         };
       } else if ('content' in current && typeof current.content === 'object') {
         const contentKeys = Object.keys(current.content);
