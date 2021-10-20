f5aa2f40b6f2b2bbbfbd13bdf8a74dfa64d7a053
Jon Ursenbach <jon@ursenba.ch>
2021-10-20 11:51:19 -0700

    fix: updating the swagger petstore yaml example to match its json counterpart

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,6 +7,13 @@ describe.each([
   ['OpenAPI 3.0', 'openapi', '3.0'],
   ['OpenAPI 3.1', 'openapi', '3.1'],
 ])('%s', (_, specification, version) => {
+  it('should have parity between JSON and YAML petstores', async () => {
+    const json = await OpenAPIParser.validate(`${version}/json/petstore.json`);
+    const yaml = await OpenAPIParser.validate(`${version}/yaml/petstore.yaml`);
+
+    expect(json).toStrictEqual(yaml);
+  });
+
   describe('JSON', () => {
     it.each(fg.sync([`${version}/json/*.json`]).map(file => [path.basename(file), file]))(
       'should validate `%s` as valid',
