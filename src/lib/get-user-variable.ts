696e7677092d257b5cd79549bd0708fef4dfd37c
Jon Ursenbach <jon@ursenba.ch>
2021-11-02 12:08:28 -0700

    feat: rewriting `get-user-variable` in typescript

---
+++ src/lib/get-user-variable.ts
@@ -0,0 +1,19 @@
+export type UserVariable = unknown | null;
+
+function getKey(key: User | Record<string, primitive>, property: string): UserVariable {
+  return key[property] || null;
+}
+
+export default function getUserVariable(user: User, property: string, selectedApp?: primitive): UserVariable {
+  let key = user;
+
+  if ('keys' in user && Array.isArray(user.keys) && user.keys.length) {
+    if (selectedApp) {
+      key = user.keys.find(k => k.name === selectedApp);
+    } else {
+      key = user.keys[0];
+    }
+  }
+
+  return getKey(key, property);
+}
