f4007f61f7d25936dba0a6f6e5941af9af1fd55f
Jon Ursenbach <jon@ursenba.ch>
2022-01-16 18:18:17 -0800

    fix: dropping the `getSchema` library

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import findSchemaDefinition from './lib/find-schema-definition';
-import getSchema from './lib/get-schema';
 import matchesMimeType from './lib/matches-mimetype';
 import { types as jsonSchemaTypes } from './operation/get-parameters-as-json-schema';
 
@@ -7,7 +6,6 @@ const supportedMethods = new Set(['get', 'put', 'post', 'delete', 'options', 'he
 
 export default {
   findSchemaDefinition,
-  getSchema,
   jsonSchemaTypes,
   matchesMimeType,
 };
