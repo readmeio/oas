9740e7b20f5b3105d87539c6dbea477853596960
Jon Ursenbach <jon@ursenba.ch>
2022-01-24 11:08:50 -0800

    fix: bring back getSchema but deprecating it

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import findSchemaDefinition from './lib/find-schema-definition';
+import getSchema from './lib/get-schema';
 import matchesMimeType from './lib/matches-mimetype';
 import { types as jsonSchemaTypes } from './operation/get-parameters-as-json-schema';
 
@@ -6,6 +7,7 @@ const supportedMethods = new Set(['get', 'put', 'post', 'delete', 'options', 'he
 
 export default {
   findSchemaDefinition,
+  getSchema,
   jsonSchemaTypes,
   matchesMimeType,
 };
