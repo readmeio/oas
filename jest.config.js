93dd5e3a81e5edabd754502948ece9682c42ea14
Jon Ursenbach <jon@ursenba.ch>
2022-09-09 16:23:40 -0700

    feat: jest29 compat

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
   coveragePathIgnorePatterns: ['__tests__/__fixtures__/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '__tests__/tsconfig.json',
-    },
-  },
   preset: 'ts-jest/presets/js-with-ts',
   testPathIgnorePatterns: ['__tests__/__fixtures__/'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
+  transform: {
+    '\\.ts$': ['ts-jest', { tsconfig: '__tests__/tsconfig.json' }],
+  },
 };
