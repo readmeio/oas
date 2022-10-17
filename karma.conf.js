cff718e0b3beab82cef38b084e3dddbb536f9070
Jon Ursenbach <jon@ursenba.ch>
2022-10-17 09:13:22 -0700

    chore(deps-dev): bumping out of date deps

--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,12 @@
 // https://karma-runner.github.io/0.12/config/configuration-file.html
 // https://jstools.dev/karma-config/
 
-"use strict";
-
-const { karmaConfig } = require("@jsdevtools/karma-config");
-const { host } = require("@jsdevtools/host-environment");
+const { host } = require('@jsdevtools/host-environment');
+const { karmaConfig } = require('@jsdevtools/karma-config');
 
 module.exports = karmaConfig({
-  sourceDir: "lib",
-  fixtures: "test/fixtures/**/*.js",
+  sourceDir: 'lib',
+  fixtures: 'test/fixtures/**/*.js',
   browsers: {
     chrome: true,
     firefox: true,
@@ -21,7 +19,7 @@ module.exports = karmaConfig({
     exclude: [
       // Exclude these tests because some of the APIs are HUGE and cause timeouts.
       // We still test them in Node though.
-      "test/specs/real-world/*",
-    ]
+      'test/specs/real-world/*',
+    ],
   },
 });
