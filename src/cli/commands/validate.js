f8d462582a290f9bd86fa359d0810e2640ce5202
Jon Ursenbach <jon@ursenba.ch>
2022-10-27 15:35:09 -0700

    feat: sunsetting the CLI

--- src/cli/commands/validate.js
+++
@@ -1,9 +0,0 @@
-const chalk = require('chalk');
-
-exports.swagger = true;
-exports.desc = 'Validate your OpenAPI definition';
-
-exports.run = function () {
-  console.log(`${chalk.green('✔ Success!')} Your OpenAPI definition is valid!`);
-  process.exit();
-};
