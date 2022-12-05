943a8d9b444f775143cb2201191ffa180d69953d
Jon Ursenbach <jon@ursenba.ch>
2022-12-05 12:20:32 -0800

    chore(deps-dev): bumping jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   coveragePathIgnorePatterns: ['/dist', '/node_modules', '/__tests__/__fixtures__/', '/__tests__/(.*)/__fixtures__/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '__tests__/tsconfig.json',
-    },
-  },
   modulePaths: ['<rootDir>'],
-  preset: 'ts-jest/presets/js-with-ts',
   roots: ['<rootDir>'],
   testPathIgnorePatterns: ['/__tests__/__fixtures__/', '/__tests__/(.*)/__fixtures__/'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
-  transform: {},
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '__tests__/tsconfig.json',
+      },
+    ],
+  },
 };
