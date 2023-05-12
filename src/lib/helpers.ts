20535b17ac18a51c2449fb4e61cd36fd8575b8f2
Jon Ursenbach <jon@ursenba.ch>
2023-05-12 10:44:51 -0700

    chore: minor annoying spacing fix

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -7,6 +7,7 @@ export function hasSchemaType(schema: SchemaObject, discriminator: 'array' | 'ob
 
   return schema.type === discriminator;
 }
+
 export function isPrimitive(val: unknown): boolean {
   return typeof val === 'string' || typeof val === 'number' || typeof val === 'boolean';
 }
