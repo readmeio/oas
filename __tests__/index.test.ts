d57af07a47a219c4312ed24e7e6e8d9215608061
Gabe Ratcliff <gaberatcliff@gmail.com>
2023-08-16 10:08:45 -0700

    feat: production ready (#1)
    Co-authored-by: Jon Ursenbach <jon@ursenba.ch>

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,20 +1,71 @@
-import { retrieveMarkdown, normalizeHeader } from '../src';
+import nock from 'nock';
+import { describe, it, expect, afterEach } from 'vitest';
+
+import * as httpHeaders from '../src';
+
+const { default: getHeaderDescription, interpolateDescription, sourceUrl } = httpHeaders;
 
 describe('HTTP Headers', () => {
   describe('#retrieveMarkdown', () => {
     it('should request source markdown from MDN', async () => {
-      const markdown = await retrieveMarkdown();
+      const markdown = await httpHeaders.retrieveMarkdown();
       expect(markdown).toContain('title: HTTP headers');
     });
   });
 
-  describe('#normalizeHeader', () => {
-    it('should convert a string into header identification format', () => {
-      expect(normalizeHeader('Authorization')).toBe('');
+  describe('#interpolateDescription', () => {
+    it('should interpolate a complex glossary term', () => {
+      const input = 'Informs the server about the {{Glossary("MIME_type", "types")}} of data that can be sent back.';
+      expect(interpolateDescription(input)).toBe('Informs the server about the types of data that can be sent back.');
     });
 
-    it('should capitalize lowercase headers', () => {
-      expect(normalizeHeader('content-length')).toBe('');
+    it('should interpolate a simple glossary term', () => {
+      const input =
+        'The {{Glossary("effective connection type")}} ("network profile") that best matches the connection\'s latency and bandwidth.';
+      expect(interpolateDescription(input)).toBe(
+        'The effective connection type ("network profile") that best matches the connection\'s latency and bandwidth.',
+      );
+    });
+
+    it('should return existing description if no regexp matches', () => {
+      const input = "Approximate bandwidth of the client's connection to the server, in Mbps.";
+      expect(interpolateDescription(input)).toBe(input);
+    });
+  });
+
+  describe('#getHeaderDescription', () => {
+    afterEach(() => {
+      nock.restore();
+    });
+
+    it('should return an empty object if something goes wrong', async () => {
+      const mock = nock(sourceUrl).get('').reply(500);
+      const headers = 'Connection';
+      const descriptions = await getHeaderDescription(headers);
+      expect(descriptions).toStrictEqual({});
+      mock.done();
+    });
+
+    it('should return a header description for a string argument', async () => {
+      const headers = 'Connection';
+      const descriptions = await getHeaderDescription(headers);
+      expect(descriptions).toStrictEqual({
+        Connection: 'Controls whether the network connection stays open after the current transaction finishes.',
+      });
+    });
+
+    it('should return header descriptions for an array of strings', async () => {
+      const headers = ['authorization', 'accept', 'Content-Security-Policy', 'nel', 'ECT', 'Accept-Encoding'];
+      const descriptions = await getHeaderDescription(headers);
+      expect(descriptions).toStrictEqual({
+        accept: 'Informs the server about the types of data that can be sent back.',
+        'Accept-Encoding':
+          'The encoding algorithm, usually a compression algorithm, that can be used on the resource sent back.',
+        authorization: 'Contains the credentials to authenticate a user-agent with a server.',
+        'Content-Security-Policy': 'Controls resources the user agent is allowed to load for a given page.',
+        nel: 'Defines a mechanism that enables developers to declare a network error reporting policy.',
+        ECT: 'The effective connection type ("network profile") that best matches the connection\'s latency and bandwidth. This is part of the Network Information API.',
+      });
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,57 +6,126 @@ import { find } from 'unist-util-find';
 import { findAfter } from 'unist-util-find-after';
 import flatFilter from 'unist-util-flat-filter';
 
-const sourceUrl = 'https://raw.githubusercontent.com/mdn/content/main/files/en-us/web/http/headers/index.md';
+// Core Resource - Sourcing headers directly from MDN
+export const sourceUrl = 'https://raw.githubusercontent.com/mdn/content/main/files/en-us/web/http/headers/index.md';
+// Store MDN response in memory for future lookups
 let markdown: string;
+// Store previously looked-up header descriptions in memory
+const cachedHTTPHeaders: Record<string, string> = {};
 
-interface ChildTextNode extends Node {
-  value: string;
-}
-
-export async function retrieveMarkdown() {
-  try {
-    const response = await fetch(sourceUrl);
-    markdown = await response.text();
-  } catch (e) {
-    return '';
-  }
-
-  return markdown;
-}
-
-export const normalizeHeader = (value: string) => {
-  const normalizedValue = value
-    .split('-')
-    .map(v => v.charAt(0).toUpperCase() + v.slice(1))
-    .join('-');
-
-  return `{{HTTPHeader("${normalizedValue}")}}`;
+/**
+ * Fetch MDN HTTP Header source markdown.
+ */
+export const retrieveMarkdown = (): Promise<string> => {
+  return fetch(sourceUrl).then(res => {
+    if (!res.ok) throw new Error('Failed to fetch markdown.');
+    return res.text();
+  });
 };
 
-export const searchHeaderDescription = (tree: Parent, value: string) => {
-  if (tree && value) {
-    const headerNode = find(tree as any, { value: normalizeHeader(value) });
+/**
+ * Normalizes and converts a header into markdown-representative identifier
+ * @example
+ * normalizeHeader('content-length') -> '{{HTTPHeader("content-length")}}'
+ */
+export const normalizeHeader = (header: string): string => `{{HTTPHeader("${header}")}}`.toLowerCase();
+
+/**
+ * Builds complete HTTP header description
+ * Full description may be multi-line. Ensure we've captured complete text before returning.
+ */
+export const buildDescription = (tree: Parent, headerNode: ChildTextNode): string => {
+  let description = '';
+  let currentNode = headerNode;
+
+  while (!description.endsWith('.')) {
+    currentNode = findAfter(tree, currentNode, { type: 'text' }) as ChildTextNode;
+    if (currentNode?.value) {
+      const [fallback, text] = currentNode.value.split(': ') ?? [];
+      description = description.concat(text || fallback || '');
+    }
+  }
+
+  return description;
+};
+
+/**
+ * Interpolates description string content
+ * @example
+ * interpolateDescription('{{Glossary("MIME_type", "types")}}') => 'types'
+ */
+export const interpolateDescription = (description: string): string => {
+  // i.e. '{{Glossary("effective connection type")}}'
+  const simpleGlossaryRegexp = /\{\{Glossary\("([^"]+)"?\)\}\}/g;
+  // i.e. '{{Glossary("MIME_type", "types")}}'
+  const compoundGlossaryRegexp = /\{\{Glossary\("([^"]+)", "([^"]+)"\)\}\}/g;
+
+  // Prematurely return if nothing to process
+  if (!description) return description;
+
+  if (description.match(simpleGlossaryRegexp)) return description.split(simpleGlossaryRegexp).join('');
+
+  if (description.match(compoundGlossaryRegexp)) {
+    const [start, , interpolation, end] = description.split(compoundGlossaryRegexp);
+    return `${start}${interpolation}${end}`;
+  }
+
+  return description;
+};
+
+interface ChildTextNode extends Node {
+  value?: string;
+}
+
+/**
+ * Performs lookup in MDN HTTP Header markdown for target header.
+ * Returns a description, if found.
+ */
+export const searchHeaderDescription = (tree: Parent, header: string): string => {
+  if (tree && header) {
+    const headerNode = find(tree as any, (node: ChildTextNode) => {
+      return (
+        (node.value?.toLowerCase().includes(normalizeHeader(header)) && node.position?.start.column === 3) || false
+      );
+    });
 
     if (headerNode) {
-      const descriptionNode = findAfter(tree, headerNode, { type: 'text' }) as ChildTextNode;
-      if (descriptionNode?.value) return descriptionNode.value.split(': ')?.[1];
+      const description = buildDescription(tree, headerNode);
+      return interpolateDescription(description);
     }
   }
   return '';
 };
 
-export default async function getHeaderDescription(header: string | string[]) {
+/**
+ * Performs lookup in MDN HTTP Header markdown for target list of HTTP headers.
+ */
+export default async function getHeaderDescription(header: string | string[]): Promise<Record<string, string>> {
   try {
-    if (!markdown) await retrieveMarkdown();
+    // If markdown has not been requested and cached, do so now
+    if (!markdown) markdown = await retrieveMarkdown();
 
+    // Convert text -> markdown syntax tree
     const mdast = fromMarkdown(markdown, 'utf-8') as any;
+    // Flatten tree for easy value enumeration
     const tree = flatFilter(mdast, node => node?.type === 'text') as Parent;
-
+    // Convert args into a unified format
     const headers = Array.isArray(header) ? header : [header];
-    return headers.reduce((acc, h) => {
-      acc[h] = searchHeaderDescription(tree, h);
-      return acc;
-    }, {} as Record<string, string>);
+
+    // Process headers and apply found descriptions
+    return headers.reduce(
+      (acc, h) => {
+        if (cachedHTTPHeaders[h]) {
+          acc[h] = cachedHTTPHeaders[h];
+        } else {
+          const description = searchHeaderDescription(tree, h);
+          cachedHTTPHeaders[h] = description;
+          acc[h] = description;
+        }
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
   } catch (e) {
     return {};
   }
