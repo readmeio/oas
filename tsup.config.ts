0421caadacbbebc4776661216fef387aee869c20
Jon Ursenbach <jon@ursenba.ch>
2023-09-01 15:12:29 -0700

    chore: cleaning up some configs

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,15 +5,23 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   ...options,
-  entry: ['src/index.ts', 'src/operation.ts', 'src/rmoas.types.ts', 'src/analyzer/index.ts', 'src/lib/reducer.ts'],
-  treeshake: true,
-  format: ['esm', 'cjs'],
-  silent: !options.watch,
-  minify: false,
-  dts: true,
-  sourcemap: true,
-  clean: true,
-  splitting: true,
-  shims: true,
+
   cjsInterop: true,
+  clean: true,
+  dts: true,
+  entry: [
+    'src/analyzer/index.ts',
+    'src/lib/reducer.ts',
+    'src/index.ts',
+    'src/operation.ts',
+    'src/rmoas.types.ts',
+    'src/utils.ts',
+  ],
+  format: ['esm', 'cjs'],
+  minify: false,
+  shims: true,
+  silent: !options.watch,
+  sourcemap: true,
+  splitting: true,
+  treeshake: true,
 }));
