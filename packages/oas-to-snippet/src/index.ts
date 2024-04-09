61b5348e64fb85e2f9ed78a2944000b5e9c6b872
Jon Ursenbach <jon@ursenba.ch>
2024-04-09 11:37:24 -0700

    feat(oas-to-snippet): exporting the `HarRequest` type for use

--- packages/oas-to-snippet/src/index.ts
+++ packages/oas-to-snippet/src/index.ts
@@ -1,5 +1,5 @@
 import type { Language } from './languages.js';
-import type { HarRequest } from '@readme/httpsnippet';
+import type { HarRequest } from './types.js';
 import type { ClientPlugin, TargetId } from '@readme/httpsnippet/targets';
 import type { AuthForHAR, DataForHAR } from '@readme/oas-to-har/lib/types';
 import type Oas from 'oas';
