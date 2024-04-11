c6177e23c478a2a3e19aee31394a9a947d4b7451
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2024-04-11 12:20:50 -0500

    fix(oas-to-snippet): use correct val for registry identifier (#861)
    oops lol (my fault too, i should have looked more closely at the
    snapshots)

--- packages/oas-to-snippet/src/index.ts
+++ packages/oas-to-snippet/src/index.ts
@@ -129,7 +129,7 @@ export default function oasToSnippet(
     }
   });
 
-  const install = getClientInstallationInstructions(languages, lang, opts?.openapi?.variableName) || false;
+  const install = getClientInstallationInstructions(languages, lang, opts?.openapi?.registryIdentifier) || false;
 
   try {
     const code = snippet.convert(language, target, targetOpts);
