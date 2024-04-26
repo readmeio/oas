49345bbd839668e6c0389a52ac42ac074ca8c3e1
Kanad Gupta <git@kanad.dev>
2024-04-25 19:24:36 -0500

    refactor: shuffle around a type
    that way we can use it in this new handler?

--- packages/oas-to-har/src/lib/types.ts
+++ packages/oas-to-har/src/lib/types.ts
@@ -1,17 +1,7 @@
 export type AuthForHAR = Record<string, number | string | { pass?: string; user?: string }>;
 
-export interface DataForHAR {
-  body?: any;
-  cookie?: Record<string, any>;
-  formData?: Record<string, any>; // `application/x-www-form-urlencoded` requests payloads.
-  header?: Record<string, any>;
-  path?: Record<string, any>;
-  query?: Record<string, any>;
-  server?: {
-    selected: number;
-    variables?: Record<string, unknown>;
-  };
-}
+export { DataForHAR } from 'oas/types';
+
 export interface oasToHarOptions {
   // If true, the operation URL will be rewritten and prefixed with https://try.readme.io/ in
   // order to funnel requests through our CORS-friendly proxy.
--- packages/oas/src/types.ts
+++ packages/oas/src/types.ts
@@ -19,6 +19,24 @@ export function isOAS31(check: OpenAPIV3_1.Document | OpenAPIV3.Document): check
   return check.openapi === '3.1.0';
 }
 
+/**
+ * Data shape for taking OpenAPI operation data and converting it into HAR.
+ *
+ * @see {@link https://github.com/readmeio/oas/tree/main/packages/oas-to-har}
+ */
+export interface DataForHAR {
+  body?: any;
+  cookie?: Record<string, any>;
+  formData?: Record<string, any>; // `application/x-www-form-urlencoded` requests payloads.
+  header?: Record<string, any>;
+  path?: Record<string, any>;
+  query?: Record<string, any>;
+  server?: {
+    selected: number;
+    variables?: Record<string, unknown>;
+  };
+}
+
 export interface User {
   [key: string]: unknown;
   keys?: {
