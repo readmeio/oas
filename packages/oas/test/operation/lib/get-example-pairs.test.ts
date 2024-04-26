c9877bc7914d0e69334ff6814879ca75e6cd3b4f
Kanad Gupta <git@kanad.dev>
2024-04-25 19:26:26 -0500

    initial tests

---
+++ packages/oas/test/operation/lib/get-example-pairs.test.ts
@@ -0,0 +1,30 @@
+import { beforeAll, test, expect } from 'vitest';
+
+import Oas from '../../../src/index.js';
+
+let customExtensions: Oas;
+let requestExamples: Oas;
+
+beforeAll(async () => {
+  customExtensions = await import('@readme/oas-examples/3.0/json/readme-extensions.json')
+    .then(r => r.default)
+    .then(Oas.init);
+  await customExtensions.dereference();
+
+  requestExamples = await import('@readme/oas-examples/3.0/json/request-examples.json')
+    .then(r => r.default)
+    .then(Oas.init);
+  await requestExamples.dereference();
+});
+
+test('mixed', () => {
+  const operation = requestExamples.operation('/parameterExamples/{param1}/{param2}', 'patch');
+  const pairs = operation.getExamplePairs();
+  expect(pairs).toMatchSnapshot();
+});
+
+test('custom code samples', () => {
+  const operation = customExtensions.operation('/x-code-samples', 'post');
+  const pairs = operation.getExamplePairs();
+  expect(pairs).toMatchSnapshot();
+});
