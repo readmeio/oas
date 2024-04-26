f792770d31e5064a8a7dd382a0821e68527b2d3b
Kanad Gupta <git@kanad.dev>
2024-04-26 14:10:15 -0500

    test: more coverage

--- packages/oas/test/operation/lib/get-example-groups.test.ts
+++ packages/oas/test/operation/lib/get-example-groups.test.ts
@@ -4,6 +4,7 @@ import Oas from '../../../src/index.js';
 
 let customExtensions: Oas;
 let requestExamples: Oas;
+let trainTravel: Oas;
 
 beforeAll(async () => {
   customExtensions = await import('@readme/oas-examples/3.0/json/readme-extensions.json')
@@ -11,16 +12,31 @@ beforeAll(async () => {
     .then(Oas.init);
   await customExtensions.dereference();
 
+  trainTravel = await import('@readme/oas-examples/3.1/json/train-travel.json').then(r => r.default).then(Oas.init);
+  await trainTravel.dereference();
+
   requestExamples = await import('@readme/oas-examples/3.0/json/request-examples.json')
     .then(r => r.default)
     .then(Oas.init);
   await requestExamples.dereference();
 });
 
-test('mixed', () => {
+test('body and path param examples with matching response examples', () => {
   const operation = requestExamples.operation('/parameterExamples/{param1}/{param2}', 'patch');
   const pairs = operation.getExampleGroups();
   expect(pairs).toMatchSnapshot();
+});
+
+test('body param examples with matching response examples', () => {
+  const operation = trainTravel.operation('/bookings/{bookingId}/payment', 'post');
+  const pairs = operation.getExampleGroups();
+  expect(pairs).toMatchSnapshot();
+});
+
+test('body param example with no title to match responses against', () => {
+  const operation = trainTravel.operation('/bookings', 'post');
+  const pairs = operation.getExampleGroups();
+  expect(pairs).toStrictEqual({});
 });
 
 test('custom code samples', () => {
