b640153178d4d3fd8ea771af3b7f7f8a80102eeb
Kanad Gupta <git@kanad.dev>
2024-08-30 17:15:39 -0500

    chore: comment tweak

--- packages/oas/src/operation/index.ts
+++ packages/oas/src/operation/index.ts
@@ -254,7 +254,7 @@ export class Operation {
           // Only add schemes we haven't seen yet.
           const exists = prev[security.type].some(sec => sec._key === security.security._key);
           if (!exists) {
-            // Since an operation can require the same security scheme several times each with different scope requirements
+            // Since an operation can require the same security scheme several times (each with different scope requirements),
             // including the `_requirements` in this object would be misleading since we dedupe the security schemes.
             // eslint-disable-next-line no-underscore-dangle
             if (security.security?._requirements) delete security.security._requirements;
