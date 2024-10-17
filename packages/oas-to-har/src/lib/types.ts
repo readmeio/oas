d8ed4d33c13737121473f39fcc4c4e167374c977
Bill Mill <bill@billmill.org>
2024-10-17 13:08:23 -0400

    docs: // -> /* */

--- packages/oas-to-har/src/lib/types.ts
+++ packages/oas-to-har/src/lib/types.ts
@@ -3,9 +3,10 @@ export type AuthForHAR = Record<string, number | string | { pass?: string; user?
 export { DataForHAR } from 'oas/types';
 
 export interface oasToHarOptions {
-  // the URL of a proxy to use. Requests will be preefixed with its value; for
-  // example if you use "https://try.readme.io", a request to
-  // "https://example.com/some/api" will be sent to
-  // "https://try.readme.io/https://example.com/some/api"
+  /* the URL of a proxy to use. Requests will be preefixed with its value; for
+   * example if you use "https://try.readme.io", a request to
+   * "https://example.com/some/api" will be sent to
+   * "https://try.readme.io/https://example.com/some/api"
+   */
   proxyUrl: string;
 }
