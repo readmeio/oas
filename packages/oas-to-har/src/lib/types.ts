967a6d07f1631068eae25d085529738e47edfe60
Kanad Gupta <git@kanad.dev>
2024-10-17 14:05:56 -0500

    docs: smol typo fix

--- packages/oas-to-har/src/lib/types.ts
+++ packages/oas-to-har/src/lib/types.ts
@@ -3,7 +3,8 @@ export type AuthForHAR = Record<string, number | string | { pass?: string; user?
 export { DataForHAR } from 'oas/types';
 
 export interface oasToHarOptions {
-  /* the URL of a proxy to use. Requests will be preefixed with its value; for
+  /**
+   * The URL of a proxy to use. Requests will be prefixed with its value; for
    * example if you use "https://try.readme.io", a request to
    * "https://example.com/some/api" will be sent to
    * "https://try.readme.io/https://example.com/some/api"
