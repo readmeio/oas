b824ac6c3b1c5ee10d3a64890378f9f23c00fafa
Jon Ursenbach <erunion@users.noreply.github.com>
2025-01-30 11:25:23 -0800

    chore: dropping `sinon` for native vitest spies (#288)
    ## 🧰 Changes
    
    This drops our unit test dependency on `sinon` in favor of native Vitest
    spies.

--- test/specs/oas-relative-servers/v3-relative-servers.test.ts
+++ test/specs/oas-relative-servers/v3-relative-servers.test.ts
@@ -1,6 +1,7 @@
+import type { MockInstance } from 'vitest';
+
 import $RefParser from '@readme/json-schema-ref-parser';
-import sinon from 'sinon';
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest';
 
 import OpenAPIParser from '../../../lib';
 import path from '../../utils/path';
@@ -17,32 +18,26 @@ const RELATIVE_SERVERS_OAS3_URL_1 = 'https://petstore3.swagger.io/api/v3/openapi
 const RELATIVE_SERVERS_OAS3_URL_2 = 'https://foo.my.cloud/v1/petstore/relativeservers';
 
 describe('Servers with relative paths in OpenAPI v3 files', () => {
-  let mockParse;
+  let spy: MockInstance<typeof $RefParser.prototype.parse>;
 
   beforeEach(() => {
-    // Mock the parse function
-    mockParse = sinon.stub($RefParser.prototype, 'parse');
+    spy = vi.spyOn($RefParser.prototype, 'parse');
   });
 
   afterEach(() => {
-    // Restore the parse function
-    $RefParser.prototype.parse.restore();
+    vi.restoreAllMocks();
   });
 
   it('should fix relative servers path in the file fetched from url', async () => {
-    mockParse.callsFake(() => {
-      // to prevent edit of the original JSON
-      return JSON.parse(JSON.stringify(v3RelativeServerJson));
-    });
+    spy.mockImplementationOnce(() => JSON.parse(JSON.stringify(v3RelativeServerJson)));
+
     const apiJson = await OpenAPIParser.parse(RELATIVE_SERVERS_OAS3_URL_1);
     expect(apiJson.servers[0].url).to.equal('https://petstore3.swagger.io/api/v3');
   });
 
   it('should fix relative servers at root, path and operations level in the file fetched from url', async () => {
-    mockParse.callsFake(() => {
-      // to prevent edit of the original JSON
-      return JSON.parse(JSON.stringify(v3RelativeServerPathsOpsJson));
-    });
+    spy.mockImplementationOnce(() => JSON.parse(JSON.stringify(v3RelativeServerPathsOpsJson)));
+
     const apiJson = await OpenAPIParser.parse(RELATIVE_SERVERS_OAS3_URL_2);
     expect(apiJson.servers[0].url).to.equal('https://foo.my.cloud/api/v3');
     expect(apiJson.paths['/pet'].servers[0].url).to.equal('https://foo.my.cloud/api/v4');
@@ -50,9 +45,8 @@ describe('Servers with relative paths in OpenAPI v3 files', () => {
   });
 
   it('should parse but no change to relative servers path in local file import', async () => {
-    mockParse.callsFake(() => {
-      return JSON.parse(JSON.stringify(v3RelativeServerPathsOpsJson));
-    });
+    spy.mockImplementationOnce(() => JSON.parse(JSON.stringify(v3RelativeServerPathsOpsJson)));
+
     const apiJson = await OpenAPIParser.parse(path.rel('./v3-relative-server.json'));
     expect(apiJson.servers[0].url).to.equal('/api/v3');
     expect(apiJson.paths['/pet'].servers[0].url).to.equal('/api/v4');
@@ -60,9 +54,8 @@ describe('Servers with relative paths in OpenAPI v3 files', () => {
   });
 
   it('should parse but no change to non-relative servers path in local file import', async () => {
-    mockParse.callsFake(() => {
-      return JSON.parse(JSON.stringify(v3NonRelativeServerJson));
-    });
+    spy.mockImplementationOnce(() => JSON.parse(JSON.stringify(v3NonRelativeServerJson)));
+
     const apiJson = await OpenAPIParser.parse(path.rel('./v3-non-relative-server.json'));
     expect(apiJson.servers[0].url).to.equal('https://petstore3.swagger.com/api/v3');
     expect(apiJson.paths['/pet'].servers[0].url).to.equal('https://petstore3.swagger.com/api/v4');
