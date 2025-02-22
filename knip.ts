4377869beac7f0df3dbd2d27747f0e698ad16424
Jon Ursenbach <jon@ursenba.ch>
2025-02-21 16:18:32 -0800

    fix: heavily slimming down our knip config

--- knip.ts
+++ knip.ts
@@ -6,37 +6,13 @@ const config: KnipConfig = {
   // `attw` is used to ensure that our dists have proper typings.
   ignoreBinaries: ['attw'],
 
-  workspaces: {
-    'packages/oas': {
-      entry: [
-        'src/index.ts',
-        'src/analyzer/index.ts',
-        'src/analyzer/types.ts',
-        'src/extensions.ts',
-        'src/operation/index.ts',
-        'src/operation/lib/get-parameters-as-json-schema.ts',
-        'src/reducer/index.ts',
-        'src/utils.ts',
-        'src/types.ts',
-      ],
-    },
-    'packages/oas-normalize': {
-      entry: ['src/index.ts', 'src/lib/types.ts', 'src/lib/utils.ts'],
-    },
-    'packages/oas-to-har': {
-      entry: ['src/index.ts', 'src/lib/configure-security.ts', 'src/lib/types.ts'],
-    },
-    'packages/oas-to-snippet': {
-      entry: ['src/index.ts', 'src/languages.ts', 'src/types.ts'],
-      ignoreDependencies: ['@types/har-format'],
-    },
-    'packages/parser': {
-      entry: ['src/index.ts'],
-    },
-    'packages/postman-types': {
-      entry: ['src/index.ts'],
-    },
-  },
+  ignoreDependencies: [
+    // This is pulled in for `oas-to-har` via `import 'har-format'` in order to package up and
+    // export some HAR typings.
+    '@types/har-format',
+  ],
+
+  // workspaces: ['packages/*'],
 };
 
 export default config;
