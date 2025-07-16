6ba2633d36efc38741b33c9578e048cc45f3ab8f
Jon Ursenbach <erunion@users.noreply.github.com>
2025-07-16 11:03:58 -0700

    chore(deps): upgrading `@apidevtools/json-scheam-ref-parser` (#988)
    ## 🧰 Changes
    
    This upgrade `@apidevtools/json-schema-ref-parser` to the latest release
    and also adds a test for the problem that was surfaced in
    https://github.com/readmeio/rdme/issues/1306 and fixed in
    https://github.com/APIDevTools/json-schema-ref-parser/pull/393.
    
    Since this upgrade had to be reverted earlier today due to it being
    released in a patch release, which was automatically pulled down to all
    existing rdme versions, since this is a major version bump of
    `json-schema-ref-parser` I'll do a major version bump of our parser
    library too. Better safe than sorry.

---
+++ packages/parser/test/specs/bundling/bundling.test.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect } from 'vitest';
+
+import { bundle } from '../../../src/index.js';
+import { relativePath } from '../../utils.js';
+
+describe('bundle', () => {
+  it('should bundle successfully', async () => {
+    const api = await bundle(relativePath('specs/bundling/nullish-example.yaml'));
+
+    expect(api).toStrictEqual({
+      openapi: '3.0.3',
+      info: {
+        version: '1.0',
+        title: 'API definition with a nullish example property',
+      },
+      paths: {
+        '/anything': {
+          get: {
+            responses: {
+              '200': {
+                description: 'OK',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'array',
+                      items: { $ref: '#/components/schemas/User-Information' },
+                    },
+                    example: { data: { first: null, last: 'lastname' } },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      components: {
+        schemas: {
+          'User-Information': {
+            type: 'object',
+            properties: { first: { type: 'boolean' }, last: { type: 'boolean' } },
+          },
+        },
+      },
+    });
+  });
+});
