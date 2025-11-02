a4671cda93f6e04f9edbb0e55cb1c78c7bd58213
Jon Ursenbach <jon@ursenba.ch>
2025-11-01 17:09:24 -0700

    chore(deps-dev): upgrading vitest and moving from eslint to biome

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,13 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
 
 import getHeader, {
-  normalizeHeader,
-  getHeaderMarkdown,
   getHeaderDescription,
   getHeaderLink,
+  getHeaderMarkdown,
   isHeaderDeprecated,
   isHeaderExperimental,
   isHeaderValid,
+  normalizeHeader,
 } from '../src';
 
 describe('HTTP Headers', () => {
