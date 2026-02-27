5a37ac38a321f0ba77e44b29400ce6f828b2dbf1
Jon Ursenbach <jon@ursenba.ch>
2026-02-27 14:18:33 -0800

    fix: just benchmark with the default iterations

--- packages/oas/test/benchmarks/reducer.bench.ts
+++ packages/oas/test/benchmarks/reducer.bench.ts
@@ -7,33 +7,21 @@ import { OpenAPIReducer } from '../../src/reducer/index.js';
 import docusign from '../__datasets__/docusign.json' with { type: 'json' };
 
 describe('OpenAPIReducer', () => {
-  bench(
-    'petstore',
-    async () => {
-      OpenAPIReducer.init(structuredClone(petstore) as OASDocument)
-        .byOperation('/store/order/{orderId}', 'Get')
-        .reduce();
-    },
-    { warmupIterations: 2, iterations: 20 },
-  );
+  bench('petstore', async () => {
+    OpenAPIReducer.init(structuredClone(petstore) as OASDocument)
+      .byOperation('/store/order/{orderId}', 'Get')
+      .reduce();
+  });
 
-  bench(
-    'docusign (operation without circular refs)',
-    async () => {
-      OpenAPIReducer.init(docusign as OASDocument)
-        .byOperation('/v2.1/accounts/{accountId}/envelopes/{envelopeId}/views/edit', 'post')
-        .reduce();
-    },
-    { warmupIterations: 2, iterations: 20 },
-  );
+  bench('docusign (operation without circular refs)', async () => {
+    OpenAPIReducer.init(docusign as OASDocument)
+      .byOperation('/v2.1/accounts/{accountId}/envelopes/{envelopeId}/views/edit', 'post')
+      .reduce();
+  });
 
-  bench(
-    'docusign (operation with circular refs)',
-    async () => {
-      OpenAPIReducer.init(docusign as OASDocument)
-        .byOperation('/v2.1/accounts/{accountId}/envelopes/{envelopeId}', 'get')
-        .reduce();
-    },
-    { warmupIterations: 2, iterations: 20 },
-  );
+  bench('docusign (operation with circular refs)', async () => {
+    OpenAPIReducer.init(docusign as OASDocument)
+      .byOperation('/v2.1/accounts/{accountId}/envelopes/{envelopeId}', 'get')
+      .reduce();
+  });
 });
