1c0cee5241035676af906df77c859d09e0426b67
Jon Ursenbach <jon@ursenba.ch>
2026-07-06 23:50:32 -0700

    fix: more type quirks

--- packages/oas-to-har/src/lib/configure-security.ts
+++ packages/oas-to-har/src/lib/configure-security.ts
@@ -61,26 +61,26 @@ export default function configureSecurity(
   if (security.type === 'apiKey') {
     if (security.in === 'query') {
       return harValue('queryString', {
-        name: security.name as string,
+        name: security.name!,
         value: String(values[scheme]),
       });
     } else if (security.in === 'header') {
       const header = {
-        name: security.name as string,
+        name: security.name!,
         value: String(values[scheme]),
       };
 
       if (security['x-bearer-format']) {
         // Uppercase: token -> Token
         const bearerFormat = security['x-bearer-format'].charAt(0).toUpperCase() + security['x-bearer-format'].slice(1);
-        header.name = security.name as string;
+        header.name = security.name!;
         header.value = `${bearerFormat} ${header.value}`;
       }
 
       return harValue('headers', header);
     } else if (security.in === 'cookie') {
       return harValue('cookies', {
-        name: security.name as string,
+        name: security.name!,
         value: String(values[scheme]),
       });
     }
